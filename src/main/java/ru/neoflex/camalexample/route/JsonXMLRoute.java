0b477ef246ea0ff417571ea9ce0c87cadfed28e9
vpervertalov <pervertalov@gmail.com>
2022-01-12 10:34:56 +0400

    exception handler

--- src/main/java/ru/neoflex/camalexample/route/JsonXMLRoute.java
+++ src/main/java/ru/neoflex/camalexample/route/JsonXMLRoute.java
@@ -1,7 +1,9 @@
 package ru.neoflex.camalexample.route;
 
 
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import lombok.RequiredArgsConstructor;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
@@ -18,15 +20,22 @@ public class JsonXMLRoute extends RouteBuilder {
                 .bindingMode(RestBindingMode.json)
                 .enableCORS(true);
 
+        onException(UnrecognizedPropertyException.class)
+                .handled(true)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+                .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+                .setBody().constant("Invalid data");
+
         rest("/order")
-                .consumes("application/json")
                 .post()
+                    .consumes("application/json")
                     .type(OrderDTO.class)
                     .route().routeId("json-order-to-xml")
-                    .setHeader("fileName").simple("${body.orderID}")
+                    .setHeader(Exchange.FILE_NAME).simple("${body.orderID}.xml")
                     .marshal().jacksonxml(true)
                     .log("body = \n${body}")
-                    .to("file:output?fileName=${headers.fileName}.xml");
+                    .to("file:output")
+                    .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(201));
 
     }
 }
